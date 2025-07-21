324b64f607021bb055791f42d1f7e643bdffe9df
HishamFawwzy0 <hishamfawzy16@gmail.com>
2025-07-21 23:35:24 +0300

    auth adding

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,17 @@
 import { Routes } from '@angular/router';
 import { Tutors } from './pages/tutors/tutors';
 import { Services } from './pages/services/services';
+import { Login } from './pages/login/login';
+import { Register } from './pages/register/register';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'tutors', pathMatch: 'full' },
   { path: 'tutors', component: Tutors },
   { path: 'services', component: Services },
+  
+  { path: 'login', component: Login },
+  { path: 'register', component: Register },
+
   { path: '**', redirectTo: 'tutors', pathMatch: 'full' },
+
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./layouts/navbar/navbar";
 import { initFlowbite } from 'flowbite';
+import { StudentReq } from "./pages/student-req/student-req";
+import { InstructorReview } from "./pages/instructor-review/instructor-review";
 
 @Component({
   selector: 'app-root',
-  imports: [  Navbar, RouterOutlet],
+  imports: [Navbar, RouterOutlet, StudentReq, InstructorReview],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
