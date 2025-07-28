e3f402b743f5ee40aa816949eac397e16fbda94e
MoaazAbdelaziz <moaazwork1@gmail.com>
2025-07-28 16:58:44 +0300

    updated tutor request service

--- src/app/core/services/tutorRequest/tutor-request-service.ts
+++ src/app/core/services/tutorRequest/tutor-request-service.ts
@@ -1,27 +1,32 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../../../environments/environment';
+import { PaginationResult } from '../../../shared/interfaces/pagination';
+import { TutorRequest } from '../../../shared/interfaces/tutor-request';
+import { PagingParams } from '../../../shared/interfaces/paging-params';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TutorRequestService {
+  constructor() {}
 
-  constructor() { }
+  http: HttpClient = inject(HttpClient);
+  baseUrl: any = environment.apiUrl;
 
-  http:HttpClient=inject(HttpClient);
-  baseUrl:any=environment.apiUrl
-
-
-  CreateTutorRequest( tutorRequest: any):Observable<any> {
+  CreateTutorRequest(tutorRequest: any): Observable<any> {
     return this.http.post(this.baseUrl + 'TutorRequest', tutorRequest);
   }
 
+  GetTutorRequests(pagingParams: PagingParams): Observable<any> {
+    let params = new HttpParams();
 
-  GetTutorRequests(): Observable<any> {
-    return this.http.get(this.baseUrl + 'TutorRequest');
+    params = params.append('pageNumber', pagingParams.pageNumber);
+    params = params.append('pageSize', pagingParams.pageSize);
+
+    return this.http.get<PaginationResult<TutorRequest>>(
+      this.baseUrl + 'TutorRequest'
+    );
   }
-
-
 }
